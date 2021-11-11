646499f55d0fa672793dbff48d45e4fd9669b82a
valeradonchev <65622681+valeradonchev@users.noreply.github.com>
2021-11-11 10:53:09 -0500

    Light sensor test, shading circuit not yet altered

--- ShadingCircuit/ShadingCircuit/ShadingCircuit.ino
+++ ShadingCircuit/ShadingCircuit/ShadingCircuit.ino
@@ -5,12 +5,8 @@ author: Arturo Guadalupi <a.guadalupi@arduino.cc>
  */
 #include <Servo.h>
 Servo myservo;
-int up_average;
-int down_average;
-int A0_val;
-int A1_val;
-int A2_val;
-int A3_val;
+int A0_val; // bottom sensor
+int A1_val; // top sensor
 int val = 90;
 int diff;
 const int threshold = 50;
@@ -19,24 +15,17 @@ const long interval = 1000;       // interval at which to update (milliseconds)
 void setup() {
   pinMode(A0, INPUT); 
   pinMode(A1, INPUT);
-  pinMode(A2, INPUT);
-  pinMode(A3, INPUT);
   myservo.attach(11);
   myservo.write(val);
 }
 void loop() {
   A0_val = analogRead(A0); //reading data from sensors
   A1_val = analogRead(A1); 
-  A2_val = analogRead(A2);
-  A3_val = analogRead(A3);
   unsigned long currentMillis = millis();
-  
-  down_average = (A0_val + A1_val)/2; //the sum of the two lower sensors /2
-  up_average = (A2_val + A3_val)/2;    //the sum of the two upper sensors /2
-  diff = abs(up_average-down_average); //checking the difference between the two averages
+  diff = abs(A1_val-A0_val); //checking the difference between the two sensors
   if (currentMillis - previousMillis >= interval) {
     previousMillis = currentMillis;
-    if ((up_average > down_average) && (diff > threshold))
+    if ((A1_val > A0_val) && (diff > threshold))
       {
       if (val < 180) //if different from max val
         {
@@ -44,7 +33,7 @@ void loop() {
         myservo.write(val);
         }
       }
-    if((down_average > up_average) && (diff > threshold))
+    if((A0_val > A1_val) && (diff > threshold))
       {
       if (val > 0) //if different from min val
         {
