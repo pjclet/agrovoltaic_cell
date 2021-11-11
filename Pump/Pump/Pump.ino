4db6b1ca3b1a3fc6a83c67246aacf4a7d7db9a2b
valeradonchev <65622681+valeradonchev@users.noreply.github.com>
2021-11-11 11:59:49 -0500

    Update Pump.ino

--- Pump/Pump/Pump.ino
+++ Pump/Pump/Pump.ino
@@ -8,33 +8,21 @@
 
 // constants used to set a pin number:
 const int pumpPin =  PLACEHOLDER;// the number of the pump pin
-const int heaterPin =  PLACEHOLDER;// heater pin number
 
-const int cold = PLACEHOLDER; // when temperature drops below this, turn heater on
-const int hot = PLACEHOLDER; // when temperature rises above this, turn heater off
 const int dry = PLACEHOLDER; // when humidity drops below this, turn pump on
 const int wet = PLACEHOLDER; // when humidity rises above this, turn pump off
 
 
 int pumpState = LOW;             // used to set pump
-int heaterState = LOW;
 
-int temp =  PLACEHOLDER; // temperature read from thermometer
 int water = PLACEHOLDER; // humidity read from thermometer
 
 void setup() {
   // set the digital pin as output:
   pinMode(pumpPin, OUTPUT);
-  pinMode(heaterPin, OUTPUT);
 }
 
 void loop() {
-    if (temp < cold) {
-      heaterState = HIGH;
-    }
-    if (temp > hot) {
-      heaterState = LOW;
-    }
     if (water < dry) {
       pumpState = HIGH;
     }
@@ -44,6 +32,5 @@ void loop() {
 
     // set the pump with the pumpState of the variable:
     digitalWrite(pumpPin, pumpState);
-    digitalWrite(heaterPin, heaterState);
   }
 }
