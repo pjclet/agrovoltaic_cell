cb9e135bcc1190fdf6964e16a9814014392b1229
valeradonchev <65622681+valeradonchev@users.noreply.github.com>
2021-11-15 11:19:22 -0500

    Update ShadingCircuit.ino

--- ShadingCircuit/ShadingCircuit/ShadingCircuit.ino
+++ ShadingCircuit/ShadingCircuit/ShadingCircuit.ino
@@ -4,11 +4,8 @@ A simple light follower
 author: Arturo Guadalupi <a.guadalupi@arduino.cc>
  */
 #include <Servo.h>
-//#include <BH1750.h>
 #include <BH1750_WE.h>
 #include <Wire.h>
-//BH1750 lightMeter1(0x23);
-//BH1750 lightMeter2(0x5C);
 BH1750_WE lightMeter1(0x5C);
 BH1750_WE lightMeter2(0x23);
 Servo myservo;
@@ -20,40 +17,41 @@ const int threshold = 50;
 unsigned long previousMillis = 0; // will store last time servo was updated
 const long interval = 1000;       // interval at which to update (milliseconds)
 void setup() {
+  Serial.begin(9600);
   pinMode(A0, INPUT); 
   pinMode(A1, INPUT);
   myservo.attach(11);
   myservo.write(val);
   Wire.begin();
-//  lightMeter1.begin();
-//  lightMeter2.begin();
     lightMeter1.init();
     lightMeter2.init();
 }
 void loop() {
   unsigned long currentMillis = millis();
   if (currentMillis - previousMillis >= interval) {
-//    float lux1 = lightMeter1.readLightLevel();
-//    float lux2 = lightMeter2.readLightLevel();
     float lux1 = lightMeter1.getLux();
     float lux2 = lightMeter2.getLux();
+    Serial.print(lux1);
+    Serial.print("  ");
+    Serial.print(lux2);
     diff = abs(lux1 - lux2); //checking the difference between the two sensors
     previousMillis = currentMillis;
     if ((lux2 > lux1) && (diff > threshold))
       {
-      if (val < 180) //if different from max val
+      if (val < 174) //if different from max val
         {
-        val++;
+        val+=5;
         myservo.write(val);
         }
       }
     if((lux1 > lux2) && (diff > threshold))
       {
-      if (val > 0) //if different from min val
+      if (val > 6) //if different from min val
         {
-        val--;
+        val-=5;
         myservo.write(val);
         }
       }
+    Serial.println();
   }
 }
