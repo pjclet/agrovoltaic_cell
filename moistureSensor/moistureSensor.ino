9c4eaf3a87c5bcd250824d1bae7d26d1ce02e8dd
PJ <pjbclet@gmail.com>
2021-11-15 10:38:57 -0500

    moisture

---
+++ moistureSensor/moistureSensor.ino
@@ -0,0 +1,25 @@
+int sensorPin = 0;    // select the input pin for the Soil moisture sensor
+int sensorValue = 0;  // variable to store the value coming from the sensor
+ 
+int sensorVCC = 10;
+ 
+void setup() {
+  // declare the ledPin as an OUTPUT:
+   Serial.begin(9600);  
+   pinMode(sensorVCC, OUTPUT); 
+   digitalWrite(sensorVCC, LOW);
+}
+ 
+void loop() {
+  // power the sensor
+  digitalWrite(sensorVCC, HIGH);
+  delay(100); //make sure the sensor is powered
+  // read the value from the sensor:
+  sensorValue = analogRead(sensorPin); 
+  //stop power 
+  digitalWrite(sensorVCC, LOW);  
+  //wait
+  delay(60*1000);//delay time change according to your need          
+  Serial.print("sensor = " );                       
+  Serial.println(sensorValue);                   
+}
