9b4e5452666917323d89719c6d132ab27b57a0ab
valeradonchev <65622681+valeradonchev@users.noreply.github.com>
2021-11-22 10:28:25 -0500

    Update Argivoltaic.ino

--- Argivoltaic/Argivoltaic.ino
+++ Argivoltaic/Argivoltaic.ino
@@ -6,6 +6,8 @@
 // timer for all sybsystems
 const long interval = 1000; // how often subsystems are updated
 unsigned long prevMillis = 0; // when subsystems were last updated
+unsigned long currentMillis = 0; // time since start
+
 // heater subsystem
 #define ONE_WIRE_BUS 3 // Data wire is conntec to the Arduino digital pin 3
 OneWire oneWire(ONE_WIRE_BUS); // Setup a oneWire instance to communicate with any OneWire devices
@@ -19,6 +21,7 @@ const long failsafeHeaterOn = 5000; // how long heater can stay on if temp senso
 const long failsafeHeaterOff = 5000; // how long heater must stay off if temp sensor is unresponsive (millis)
 unsigned long heaterOffMillis = 0; // when heater turned off
 unsigned long heaterOnMillis = 0; // when heater turned on
+
 // shading subsystem
 BH1750_WE lightMeter1(0x5C);
 BH1750_WE lightMeter2(0x23);
@@ -28,6 +31,7 @@ int lux2; // top sensor
 int servoPos = 90; // starting servo posiiton
 int luxDiff; // difference in lux reading
 const int threshold = 50; // change angle when diff>threshold
+
 // watering system
 int moisturePin = 0;    // input pin for the Soil moisture sensor
 int pumpPin = 9;      // pin that controls pump
@@ -35,6 +39,7 @@ int moistureValue = 0;  // variable to store the value coming from the sensor
 int moistureVCC = 10;   // moisutre sensor connected to digital pin for power so it can be turned on/off
 int moistureState = LOW; // moisture sensor will be turned on/off
 const int moistureDuration = 100; // how long mousture sensor takes to read a value
+
 void setup() {
   Serial.begin(9600);
   // heater setup
@@ -55,53 +60,65 @@ void setup() {
 }
 
 void loop() {
-  unsigned long currentMillis = millis();
+  currentMillis = millis();
   if(currentMillis - prevMillis > interval){
     prevMillis = currentMillis;
     // heater loop
-    sensors.requestTemperatures(); 
-    temp = sensors.getTempCByIndex(0);
-    if ((temp < cold) and (currentMillis-heaterOffMillis>failsafeHeaterOff)) {
-      if(heaterState == LOW){
-        heaterOnMillis = currentMillis;
-      }
-      heaterState = HIGH;
-    }
-    if ((temp > hot) or (currentMillis-heaterOnMillis>failsafeHeaterOn)) {
-      if(heaterState == HIGH){
-        heaterOffMillis = currentMillis;
-      }
-      heaterState = LOW;
-    }
-    digitalWrite(heaterPin, heaterState);
+    heater();
     // shading loop
-    float lux1 = lightMeter1.getLux();
-    float lux2 = lightMeter2.getLux();
-    luxDiff = abs(lux1 - lux2);
-    if ((lux2 > lux1) && (luxDiff > threshold)) {
-      if (servoPos < 174) { //avoid setting servo above its max
-        servoPos+=5;
-        myservo.write(servoPos);
-        }
-      }
-    if((lux1 > lux2) && (luxDiff > threshold)) {
-      if (servoPos > 6) { //avoid setting servo below its min
-        servoPos-=5;
-        myservo.write(servoPos);
-        }
-      }
+    shading();
     // moisture loop
-    moistureValue = analogRead(moisturePin);  
-    moistureState = LOW;
-    digitalWrite(moistureVCC, moistureState); //stop power 
-    if (moistureValue >= 30){
-      digitalWrite(pumpPin, HIGH);        
-    } else {
-      digitalWrite(pumpPin, LOW);
-    }
+    moisture();
   }
   if((currentMillis - prevMillis > interval - moistureDuration) and (moistureState == LOW)){
     moistureState = HIGH;
     digitalWrite(moistureVCC, moistureState);
+  }
+}
+
+void heater(){
+  sensors.requestTemperatures(); 
+  temp = sensors.getTempCByIndex(0);
+  if ((temp < cold) and (currentMillis-heaterOffMillis>failsafeHeaterOff)) {
+    if(heaterState == LOW){
+      heaterOnMillis = currentMillis;
+    }
+    heaterState = HIGH;
+  }
+  if ((temp > hot) or (currentMillis-heaterOnMillis>failsafeHeaterOn)) {
+    if(heaterState == HIGH){
+      heaterOffMillis = currentMillis;
+    }
+    heaterState = LOW;
+  }
+  digitalWrite(heaterPin, heaterState);
+}
+
+void shading(){
+  float lux1 = lightMeter1.getLux();
+  float lux2 = lightMeter2.getLux();
+  luxDiff = abs(lux1 - lux2);
+  if ((lux2 > lux1) && (luxDiff > threshold)) {
+    if (servoPos < 174) { //avoid setting servo above its max
+      servoPos+=5;
+      myservo.write(servoPos);
+    }
+  }
+  if((lux1 > lux2) && (luxDiff > threshold)) {
+    if (servoPos > 6) { //avoid setting servo below its min
+      servoPos-=5;
+      myservo.write(servoPos);
+    }
+  }
+}
+
+void moisture(){
+  moistureValue = analogRead(moisturePin);  
+  moistureState = LOW;
+  digitalWrite(moistureVCC, moistureState); //stop power 
+  if (moistureValue >= 30){
+    digitalWrite(pumpPin, HIGH);        
+  } else {
+    digitalWrite(pumpPin, LOW);
   }
 }
