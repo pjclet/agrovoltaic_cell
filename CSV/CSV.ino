77c367a7a1fb1e883ff679b23f900dae8a61dd27
valeradonchev <65622681+valeradonchev@users.noreply.github.com>
2021-11-28 10:42:23 -0500

    CSV file test code, can't test without Arduino

---
+++ CSV/CSV.ino
@@ -0,0 +1,21 @@
+// constants used to set a pin number:
+//const int sensor1 = A0;
+//const int sensor2 = A1;
+//int data1, int data 2;
+int i=0;
+
+void setup() {
+  Serial.begin(9600);
+//  pinMode(sensor1, INPUT);
+//  pinMode(sensor2, INPUT);
+}
+
+void loop() {
+//    data1 = analogRead(sensor1);
+//    data2 = analogRead(sensor2);
+    i++;
+    Serial.print(i);
+    Serial.print(",");
+    Serial.println(i*2);
+    delay(1000);
+  }
---
+++ CSV/CSV.py
@@ -0,0 +1,29 @@
+import serial
+
+arduino_port = "COM5" #serial port of Arduino
+baud = 9600
+fileName="data.csv" #name of the CSV file generated
+ser = serial.Serial(arduino_port, baud)
+print("Connected to Arduino port:" + arduino_port)
+file = open(fileName, "a")
+print("Created file")
+samples = 10 #how many samples to collect
+print_labels = False
+line = 0 #start at 0 because our header is 0 (not real data)
+while line <= samples:
+    # incoming = ser.read(9999)
+    # if len(incoming) > 0:
+    if print_labels:
+        if line==0:
+            print("Printing Column Headers")
+        else:
+            print("Line " + str(line) + ": writing...")
+    getData=str(ser.readline().decode())
+    data=getData[0:][:-2]
+    print(data)
+    file.write(data + "\n") #write data with a newline
+    line = line+1
+
+print("Data collection complete!")
+file.close()
+ser.close()
