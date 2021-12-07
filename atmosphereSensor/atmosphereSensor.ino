3001398b53432d1f348ef3519444f2e063b9774a
PJ <pjbclet@gmail.com>
2021-12-07 11:14:33 -0500

    final code updates

--- atmosphereSensor/atmosphereSensor.ino
+++ atmosphereSensor/atmosphereSensor.ino
@@ -10,7 +10,7 @@ int fanState = HIGH;
 void setup() {
   Serial.begin(9600);
 
-//  Serial.println("CCS811 test");
+  Serial.println("CCS811 test");
 
   if(!ccs.begin()){
     Serial.println("Failed to start sensor! Please check your wiring.");
@@ -23,20 +23,7 @@ void setup() {
 }
 
 void loop() {
-//  if(ccs.available()){
-//    if(!ccs.readData()){
-//      Serial.print("CO2: ");
-//      Serial.print(ccs.geteCO2());
-//      Serial.print("ppm, TVOC: ");
-//      Serial.println(ccs.getTVOC());
-//    }
-//    else{
-//      Serial.println("ERROR!");
-//      while(1);
-//    }
-//  }
   atmosphere();
-  
   delay(500);
 }
 
@@ -45,30 +32,26 @@ void atmosphere(){
   if (ccs.available()){ // check if sensors are active
     if (!ccs.readData()){
 
-//      Serial.print("CO2: ");
-      Serial.print(millis());
-      Serial.print(",");
+      Serial.print("CO2: ");
       Serial.print(ccs.geteCO2());
-//      Serial.print("ppm, TVOC: ");
-//      Serial.println(ccs.getTVOC());
+      Serial.print("ppm, TVOC: ");
+      Serial.println(ccs.getTVOC());
 
       if ((fanState == HIGH) && (ccs.geteCO2() > airQualityThreshold)) {
         fanState = LOW;
         digitalWrite(fanPin, fanState);
         digitalWrite(errorLED, HIGH);
-//        Serial.println("CHANGE: Fan off");
+        Serial.println("CHANGE: Fan off");
         
       } else if ((fanState == LOW) && (ccs.geteCO2() <= airQualityThreshold)) {
         fanState = HIGH;
         digitalWrite(fanPin, fanState);
         digitalWrite(errorLED, LOW);
-//        Serial.println("CHANGE: Fan on");
+        Serial.println("CHANGE: Fan on");
       }
-      Serial.print(",");
-      Serial.print(fanState);
-      Serial.println();
+      
     } else {
-//      Serial.println("ERROR: Air Sensor Inactive");
+      Serial.println("ERROR: Air Sensor Inactive");
       for (int i = 1; i < 10; i++){
         digitalWrite(errorLED, HIGH);
         delay(100);
